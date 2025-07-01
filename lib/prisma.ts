957ecea0fe863af8dcf790edb56b23cb5c046eb8
Venu005 <venusaiyalamanchili@gmail.com>
2025-07-01 18:36:46 +0530

    chore: fix prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+// @ts-nocheck
+import { PrismaClient } from "@prisma/client";
 
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 export default prisma;
